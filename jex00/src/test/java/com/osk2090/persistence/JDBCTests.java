186ab3d1f3df924973b184ca8cc773491ebc98bd
osk2090 <osk2090@naver.com>
2021-06-25 04:43:56 +0900

    fix

---
+++ jex00/src/test/java/com/osk2090/persistence/JDBCTests.java
@@ -0,0 +1,32 @@
+package com.osk2090.persistence;
+
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.junit.Assert.fail;
+
+@Log4j
+public class JDBCTests {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testConnection() {
+		try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE",
+				"book_ex",
+				"oracleLee985622!")) {
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
