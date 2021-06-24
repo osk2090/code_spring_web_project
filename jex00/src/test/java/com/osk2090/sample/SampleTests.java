05a4f0686510b16f3239ca76e97e5d5a938688e7
osk2090 <osk2090@naver.com>
2021-06-24 16:44:25 +0900

    update

--- jex00/src/test/java/com/osk2090/sample/SampleTests.java
+++ jex00/src/test/java/com/osk2090/sample/SampleTests.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.osk2090.config.RootConfig;
+
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+//@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration(classes= {RootConfig.class})
 @Log4j
 public class SampleTests {
 
