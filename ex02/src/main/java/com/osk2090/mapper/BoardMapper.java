7e78672f90f744ddc524369386d6d91ff8e54065
osk2090 <osk2090@naver.com>
2021-07-05 11:04:47 +0900

    fix

---
+++ ex02/src/main/java/com/osk2090/mapper/BoardMapper.java
@@ -0,0 +1,12 @@
+package com.osk2090.mapper;
+
+import com.osk2090.domain.BoardVO;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface BoardMapper {
+    @Select("select * from tbl_board where bno >0")
+    public List<BoardVO> getList();
+
+}
---
+++ ex02/src/test/java/com/osk2090/mapper/BoardMapperTest.java
@@ -0,0 +1,24 @@
+package com.osk2090.mapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {com.osk2090.config.RootConfig.class})
+@Log4j
+public class BoardMapperTest {
+    @Setter(onMethod_ = @Autowired)
+    private BoardMapper mapper;
+
+    @Test
+    public void testGetList() {
+        mapper.getList().forEach(board -> log.info(board));
+    }
+}
