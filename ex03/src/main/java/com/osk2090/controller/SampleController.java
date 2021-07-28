e8a64e2854e8b7bf71bcea71e9841fbac67d5def
osk2090 <osk2090@naver.com>
2021-07-28 17:39:39 +0900

    ifx

--- ex03/src/main/java/com/osk2090/controller/SampleController.java
+++ ex03/src/main/java/com/osk2090/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.osk2090.controller;
 
+import com.osk2090.domain.SampleVO;
 import lombok.extern.log4j.Log4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,5 +16,17 @@ public class SampleController {
     public String getText() {
         log.info("MIME TYPE: " + MediaType.TEXT_PLAIN_VALUE);
         return "안녕하세요";
+    }
+
+    @GetMapping(value = "/getSample",
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE})
+    public SampleVO getSample() {
+        return new SampleVO(112, "스타", "로또");
+    }
+
+    @GetMapping(value = "/getSample2")
+    public SampleVO getSample2() {
+        return new SampleVO(113, "로켓", "라쿤");
     }
 }
