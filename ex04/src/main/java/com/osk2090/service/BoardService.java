296e7f220dbb57c1e300149a8924e1ca35c093f7
osk2090 <osk2090@naver.com>
2021-08-01 00:29:57 +0900

    add

---
+++ ex04/src/main/java/com/osk2090/service/BoardService.java
@@ -0,0 +1,22 @@
+package com.osk2090.service;
+
+import com.osk2090.domain.BoardVO;
+import com.osk2090.domain.Criteria;
+
+import java.util.List;
+
+public interface BoardService {
+    public void register(BoardVO board);
+
+    public BoardVO get(Long bno);
+
+    public boolean modify(BoardVO board);
+
+    public boolean remove(Long bno);
+
+//    public List<BoardVO> getList();
+
+    public List<BoardVO> getList(Criteria cri);
+
+    public int getTotalCount(Criteria cri);
+}
---
+++ ex04/src/main/java/com/osk2090/service/BoardServiceImpl.java
@@ -0,0 +1,60 @@
+package com.osk2090.service;
+
+import com.osk2090.domain.BoardVO;
+import com.osk2090.domain.Criteria;
+import com.osk2090.mapper.BoardMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Log4j
+@Service
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService {
+    //spring 4.3 이상에서 자동 처리
+    private BoardMapper mapper;
+
+    @Override
+    public void register(BoardVO board) {
+        log.info("register....." + board);
+        mapper.insertSelectKey(board);
+    }
+
+    @Override
+    public BoardVO get(Long bno) {
+        log.info("get....." + bno);
+        return mapper.read(bno);
+    }
+
+    @Override
+    public boolean modify(BoardVO board) {
+        log.info("modify....." + board);
+        return mapper.update(board) == 1;
+    }
+
+    @Override
+    public boolean remove(Long bno) {
+        log.info("remove....." + bno);
+        return mapper.delete(bno) == 1;
+    }
+
+//    @Override
+//    public List<BoardVO> getList() {
+//        log.info("getList");
+//        return mapper.getList();
+//    }
+
+    @Override
+    public List<BoardVO> getList(Criteria cri) {
+        log.info("get List with criteria: " + cri);
+        return mapper.getListWithPaging(cri);
+    }
+
+    @Override
+    public int getTotalCount(Criteria cri) {
+        log.info("get total count");
+        return mapper.getTotalCount(cri);
+    }
+}
